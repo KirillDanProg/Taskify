310886b4a3828bcc21098f9ea45cf48f737b54d4
KirillDanProg <kirill2898v@gmail.com>
2022-11-05 12:25:26 +0300

    Preloaders added

---
+++ src/common/preloader/loadingDots.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const LoadingDots = () => {
+    return (
+        <div className="host">
+            <div className="loading loading-0"></div>
+            <div className="loading loading-1"></div>
+            <div className="loading loading-2"></div>
+        </div>
+    );
+};
+
+export default LoadingDots;
