fa231f9d4379dec8baa60d6c9f5f30d563c80996
KirillDanProg <kirill2898v@gmail.com>
2022-11-05 12:18:24 +0300

    reusable component for adding form created

---
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -0,0 +1,82 @@
+import React, {FC, useState} from 'react';
+import {Form, Formik, FormikHelpers} from "formik";
+import {Button} from "../../common/Button";
+import TextField from '@mui/material/TextField';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import styles from "./AddItemForm.module.scss"
+
+type ErrorsType = {
+    title: string
+}
+type AddItemFormType = {
+    callback: (value: string) => void
+    placeholder: string
+}
+export const AddItemForm: FC<AddItemFormType> = ({callback, placeholder}) => {
+    const [error, setError] = useState(false)
+    const validate = (values: ErrorsType) => {
+        if (!values.title) {
+            setError(true)
+        } else {
+           error && setError(false)
+        }
+    }
+    const onSubmit = (values: ErrorsType, {setSubmitting}: FormikHelpers<ErrorsType>) => {
+        callback(values.title)
+        setSubmitting(false);
+    }
+    return (
+        <Formik
+            initialValues={{title: ""}}
+            validate={validate}
+            onSubmit={onSubmit}
+            validateOnBlur={false}
+        >
+            {(formik) => (
+                <Form className={styles.addForm}>
+                    <TextField variant="standard"
+                               error={error}
+                               margin="dense"
+                               color={'primary'}
+                               label={error ? "Required" : placeholder}
+                               {...formik.getFieldProps('title')}
+                               sx={{
+                                   backgroundColor: "#fff",
+                                   borderRadius: "5px"
+                               }}
+                    />
+                    <Button type="submit" disabled={formik.isSubmitting}>
+                        <AddBoxIcon fontSize={"medium"}/>
+                    </Button>
+                </Form>
+            )}
+        </Formik>
+    )
+
+}
+//     const [inputValue, setValue] = useState(value)
+//
+//     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+//         setValue(e.currentTarget.value)
+//     }
+//     const onBlurHandler = () => {
+//         if (inputValue.trim()) {
+//             callback(inputValue)
+//         } else {
+//             setError()
+//         }
+//
+//     }
+//     const onEnterHandler = () => {
+//
+//     }
+//
+//     return (
+//         <input value={inputValue}
+//                onChange={onChangeHandler}
+//                onBlur={onBlurHandler}
+//                onKeyDown={onEnterHandler}
+//         />
+//     );
+// };
+
