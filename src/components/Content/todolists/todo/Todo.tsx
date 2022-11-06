ec5437a6af218acc4fa7a9edf2e76b0470c991dd
KirillDanProg <kirill2898v@gmail.com>
2022-11-06 20:52:29 +0300

    tasks accordion added

--- src/components/Content/todolists/todo/Todo.tsx
+++ src/components/Content/todolists/todo/Todo.tsx
@@ -10,16 +10,16 @@ import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import {useAppDispatch, useAppSelector} from "../../../../hooks/reduxHooks";
 import {addTaskTC, fetchTasksTC} from "../../../../state/reducers/taskReducer/tasks-reducer";
 import {Tasks} from "./task/Tasks";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
 
 const TodoBox = styled.div`
-  position: relative;
-  display: flex;
+  display: inline-block;
   align-items: center;
   justify-content: center;
   flex-wrap: wrap;
-  width: 100%;
+  max-width: 400px;
   margin-bottom: 16px;
-  padding: 15px 0;
   background-color: ${props => props.theme.taskBG};
   border-radius: 20px;
   box-shadow: 2px 2px 4px lightgray;
@@ -28,28 +28,51 @@ const TodoBox = styled.div`
     text-align: center;
 
   }
+
+  @media (max-width: 500px) {
+    width: 90%;
+  }
 `
 export const Todo: FC<DomainTodolistType> = (props) => {
     const dispatch = useAppDispatch()
     const tasks = useAppSelector(state => state.tasks[props.id])
-    const removeTodolistHandler = () => {
+    const removeTodolistHandler = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        e.stopPropagation()
         dispatch(deleteTodolistTC(props.id))
     }
     const AddTaskHandler = (title: string) => {
         dispatch(addTaskTC(props.id, title))
     }
+
     useEffect(() => {
         dispatch(fetchTasksTC(props.id))
     }, [])
 
+
     return (
         <TodoBox>
-            <h3>{props.title}</h3>
-            <div className={styles.removeIcon}>
-                <RemoveCircleOutlineIcon onClick={removeTodolistHandler}/>
-            </div>
-            <AddItemForm callback={AddTaskHandler} placeholder={"add task"}/>
-            <Tasks tasks={tasks}/>
+            <Accordion sx={{all: "unset"}}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <>
+                        <h3>{props.title}</h3>
+
+                        <div className={styles.removeIcon}>
+                            <RemoveCircleOutlineIcon onClick={removeTodolistHandler}/>
+                        </div>
+                    </>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <>
+                        <AddItemForm callback={AddTaskHandler} placeholder={"add task"}/>
+                        {tasks?.length > 0 ? <Tasks tasks={tasks} status={props.entityStatus}/> : " no  tasks"}
+                    </>
+
+                </AccordionDetails>
+            </Accordion>
         </TodoBox>
     );
 };
