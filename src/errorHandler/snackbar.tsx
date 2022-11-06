e7b97ba6fcacf43070338390350d6496ddefd4d8
KirillDanProg <kirill2898v@gmail.com>
2022-11-06 20:35:21 +0300

    added: errors alerts

---
+++ src/errorHandler/snackbar.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import {useAppDispatch, useAppSelector} from "../hooks/reduxHooks";
+import {setErrorAC} from "../state/reducers/app-reducer/app-reducer";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function CustomizedSnackbars() {
+    const error = useAppSelector(state => state.app.error)
+    const dispatch = useAppDispatch()
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        dispatch(setErrorAC(null))
+    };
+
+    return (
+        <Stack spacing={2} sx={{width: '100%'}}>
+            <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
+                    {error}
+                </Alert>
+            </Snackbar>
+        </Stack>
+    );
+}
