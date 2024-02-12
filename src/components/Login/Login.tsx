eb42930a1ab745b73b5121b6e35aa700ba7cbba2
KirillDanProg <kirill2898v@gmail.com>
2024-02-12 18:01:13 +0300

    fix: navigation added to login page if user authorized

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,99 +1,98 @@
-import React, { useEffect } from 'react';
-import { useFormik } from 'formik';
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import * as yup from "yup"
-import styles from "./Login.module.scss"
-import { useAppSelector } from "../../hooks/reduxHooks";
-import styled from "styled-components";
-import { useLazyGetCaptchaQuery, useLoginMutation } from "../../features/auth/authApi";
-import { selectCaptchaUrl } from "../../features/selectors";
+import * as yup from "yup";
+import styles from "./Login.module.scss";
+import { useEffect } from "react";
+import { useFormik } from "formik";
+import { TextField, Button } from "@mui/material";
 
-const LoginTipBox = styled.div`
-  color: ${props => props.theme.textColor};
-  font-size: 20px;
-  font-weight: bold;
-
-`
+import {
+  useLazyGetCaptchaQuery,
+  useLoginMutation,
+} from "../../features/auth/authApi";
+import { useAppSelector } from "hooks/reduxHooks";
+import { Navigate, useNavigate } from "react-router-dom";
+import { selectCaptchaUrl, selectIsAuth } from "features/auth/selectors";
+import { todoApi } from "../../features/todos/todoApi";
 
 const validationSchema = yup.object({
-    email: yup
-        .string()
-        .email('Enter a valid email')
-        .required('Email is required'),
-    password: yup
-        .string()
-        .min(4, 'Password should be of minimum 8 characters length')
-        .required('Password is required'),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(4, "Password should be of minimum 8 characters length")
+    .required("Password is required"),
 });
 
 export const Login = () => {
-    const captchaUrl = useAppSelector(selectCaptchaUrl)
-    const [getCaptcha] = useLazyGetCaptchaQuery()
-    const [login, { data }] = useLoginMutation()
-    const captchaError = data?.resultCode === 10
-    useEffect(() => {
-        if (captchaError) {
-            getCaptcha()
-        }
-    }, [captchaError, getCaptcha])
+  const captchaUrl = useAppSelector(selectCaptchaUrl);
+  const [getCaptcha] = useLazyGetCaptchaQuery();
+  const [login, { data }] = useLoginMutation();
+  const captchaError = data?.resultCode === 10;
+  const isAuth = useAppSelector(selectIsAuth);
 
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            captcha: ''
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            values.password = values.password.trim()
-            login(values)
-        },
-    });
+  useEffect(() => {
+    if (captchaError) {
+      getCaptcha();
+    }
+  }, [captchaError, getCaptcha]);
 
-    return (
-        <div className={styles.formBox}>
-            <form onSubmit={formik.handleSubmit}>
-                <LoginTipBox>
-                    <div>TEST LOGIN DATA</div>
-                    <div>Email: testEmailReact@gmail.com</div>
-                    <div>Password: test123!!!</div>
-                </LoginTipBox>
-                <TextField
-                    required
-                    focused={true}
-                    label="Email"
-                    variant="filled"
-                    {...formik.getFieldProps("email")}
-                />
-                <TextField
-                    required
-                    focused={true}
-                    label="Password"
-                    variant="filled"
-                    type="password"
-                    {...formik.getFieldProps("password")}
-                />
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      captcha: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      values.password = values.password.trim();
+      await login(values);
+    },
+  });
+  if (isAuth) {
+    return <Navigate to='/' />;
+  }
+  return (
+    <div className={styles.formBox}>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          required
+          focused={true}
+          label='Email'
+          variant='filled'
+          {...formik.getFieldProps("email")}
+        />
+        <TextField
+          required
+          focused={true}
+          label='Password'
+          variant='filled'
+          type='password'
+          {...formik.getFieldProps("password")}
+        />
 
-                <Button color="primary" variant="contained" fullWidth type="submit" sx={{ cursor: "pointer" }}>
-                    Submit
-                </Button>
-                {
-                    captchaUrl
-                        ? <>
-                            <img alt={"captcha"} src={captchaUrl} />
-                            <TextField
-                                required
-                                focused={true}
-                                label="captcha"
-                                type="captcha"
-                                {...formik.getFieldProps("captcha")}
-                            />
-                        </>
-                        : null
-                }
-            </form>
-        </div>
-    );
+        <Button
+          color='primary'
+          variant='contained'
+          fullWidth
+          type='submit'
+          sx={{ cursor: "pointer" }}
+        >
+          Submit
+        </Button>
+        {captchaUrl && (
+          <>
+            <img alt={"captcha"} src={captchaUrl} />
+            <TextField
+              required
+              focused={true}
+              label='captcha'
+              type='captcha'
+              {...formik.getFieldProps("captcha")}
+            />
+          </>
+        )}
+      </form>
+    </div>
+  );
 };
-
