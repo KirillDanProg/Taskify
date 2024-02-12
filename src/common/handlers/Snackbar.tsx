ef1e23f4923aa68163048f49cd2a0d6304f328dc
KirillDanProg <kirill2898v@gmail.com>
2024-02-12 17:55:11 +0300

    fix: correct error selector added for snackbar

--- src/common/handlers/Snackbar.tsx
+++ src/common/handlers/Snackbar.tsx
@@ -1,34 +1,32 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, {AlertProps} from '@mui/material/Alert';
-import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks";
-import {selectError} from "../../features/selectors";
-import {resetError} from "../../features/app/appSlice";
+import * as React from "react";
+import { Snackbar, Stack, AlertProps } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import { useAppDispatch, useAppSelector } from "hooks/reduxHooks";
+import { selectAuthError } from "features/auth/selectors";
+import { resetError } from "common/app/appSlice";
+import { selectAppError } from "common/app/selectors";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-    props,
-    ref,
+  props,
+  ref
 ) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
 
 export function CustomSnackbar() {
-    const dispatch = useAppDispatch()
-    const error = useAppSelector(selectError)
+  const dispatch = useAppDispatch();
+  const error = useAppSelector(selectAppError);
+  const handleClose = () => {
+    dispatch(resetError());
+  };
 
-
-    const handleClose = () => {
-        dispatch(resetError())
-    };
-
-    return (
-        <Stack spacing={2} sx={{width: '100%'}}>
-            <Snackbar open={!!error} autoHideDuration={3000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
-                    {error}
-                </Alert>
-            </Snackbar>
-        </Stack>
-    );
+  return (
+    <Stack spacing={2} sx={{ width: "100%" }}>
+      <Snackbar open={!!error} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity='error' sx={{ width: "100%" }}>
+          {error}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
 }
