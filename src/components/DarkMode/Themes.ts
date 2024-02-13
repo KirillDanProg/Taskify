6fde65ae2ed124209e9d64a7754b049f728565c9
KirillDanProg <kirill2898v@gmail.com>
2024-02-13 18:16:27 +0300

    style: themes colors updated, error color added

--- src/components/DarkMode/Themes.ts
+++ src/components/DarkMode/Themes.ts
@@ -1,23 +1,25 @@
-
 export const themes = {
-    light: {
-        background: "#fff",
-        mainColor:  "rgba(84,85,255,0.89)",
-        textColor: "#000",
-        secColor: "#fefefe",
-        primary: "#fff",
-        header:"rgba(84,85,255,1)",
-        taskBG: "#f2f2f2",
-        icon: "#fff"
-    },
-    dark: {
-        background: "#0a0e17c9",
-        mainColor: "#055d88",
-        textColor: "#dbdbdb",
-        secColor: "#d4d4d4",
-        primary: "#00dac6",
-        header: "#032333",
-        taskBG: "#f2f2f2",
-        icon: "#fff910"
-    }
-}
+  light: {
+    background: "#fff",
+    mainColor: "rgba(84,85,255,1)",
+    brightMainColor: "rgba(80,50,255,1)",
+    textColor: "#000",
+    secColor: "#fefefe",
+    primary: "#fff",
+    header: "rgba(84,85,255,1)",
+    taskBG: "#f2f2f2",
+    icon: "#fff",
+    error: "rgb(255, 0, 0)",
+  },
+  dark: {
+    background: "#0a0e17c9",
+    mainColor: "#055d88",
+    textColor: "#dbdbdb",
+    secColor: "#d4d4d4",
+    primary: "#00dac6",
+    header: "#032333",
+    taskBG: "#f2f2f2",
+    icon: "#fff910",
+    error: "rgb(255, 0, 0)",
+  },
+};
