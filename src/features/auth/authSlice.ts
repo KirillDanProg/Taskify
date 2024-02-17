d2189cd9fbbf978c1d6833415c979bba1e011704
KirillDanProg <kirill2898v@gmail.com>
2024-02-17 20:44:27 +0300

    feato: addMatcher for me endpoints removed from auth slice

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,36 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { authApi } from "./authApi";
 
-const initialState = {
-    email: "",
-    login: "",
-    id: null as number | null,
-    captchaUrl: "",
+interface InitialState {
+  email: string;
+  login: string;
+  id: number | null;
+  captchaUrl: string;
 }
+
+const initialState: InitialState = {
+  email: "",
+  login: "",
+  id: null as number | null,
+  captchaUrl: "",
+};
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addMatcher(authApi.endpoints.me.matchFulfilled, (state, { payload }) => {
-                if (payload.messages.length === 0) {
-                    state.email = payload.data.email
-                    state.login = payload.data.login
-                    state.id = payload.data.id
-                } else {
-                    state.id = null
-                }
-            })
-            .addMatcher(authApi.endpoints.getCaptcha.matchFulfilled, (state, { payload }) => {
-                state.captchaUrl = payload.url
-            })
-    }
-})
-
-export type LoginDataType = {
-    email: string
-    password: string
-    captcha?: string
-}
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      authApi.endpoints.getCaptcha.matchFulfilled,
+      (state, { payload }) => {
+        state.captchaUrl = payload.url;
+      }
+    );
+  },
+});
