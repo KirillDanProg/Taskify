f3b9f1ae0449d3c6e90b540617eb93f7984844c6
KirillDanProg <kirill2898v@gmail.com>
2024-02-19 00:48:03 +0300

    feat: size options added to Icon

--- src/common/components/icon/Icon.tsx
+++ src/common/components/icon/Icon.tsx
@@ -2,8 +2,13 @@ import { ReactNode } from "react";
 import s from "./Icon.module.scss";
 type Props = {
   children: ReactNode;
+  size?: "S" | "M" | "L";
 };
 
-export const Icon = ({ children }: Props) => {
-  return <div className={s.iconContainer}>{children}</div>;
+export const Icon = ({ children, size }: Props) => {
+  return (
+    <div className={`${s.iconContainer} ${s[size ? size : "S"]}`}>
+      {children}
+    </div>
+  );
 };
