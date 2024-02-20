5e2d720eff7d653bbab406dd3f52a140aa0e3404
KirillDanProg <kirill2898v@gmail.com>
2024-02-20 21:33:06 +0300

    feat: sort todolists and align todolists position on screen added with url search params

--- src/features/todos/ui/Todolists.tsx
+++ src/features/todos/ui/Todolists.tsx
@@ -1,10 +1,22 @@
 import { Todo } from "./todo/Todo";
 import { useFetchTodoslistsQuery } from "../todoApi";
 import s from "./Todolists.module.scss";
+import { useSearchParams } from "react-router-dom";
 
 export const Todolists = () => {
   const { data: todolists = [] } = useFetchTodoslistsQuery();
-  const mappedTodolists = todolists.map(({ id, title, entityStatus }) => {
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get("sort");
+  const align = searchParams.get("align");
+  const sortedTodolists = sort
+    ? [...todolists].sort((a: any, b: any) => {
+        return sort === "asc"
+          ? a.title.localeCompare(b.title)
+          : b.title.localeCompare(a.title);
+      })
+    : todolists;
+
+  const mappedTodolists = sortedTodolists.map(({ id, title, entityStatus }) => {
     return (
       <Todo
         key={id}
@@ -12,9 +24,14 @@ export const Todolists = () => {
         id={id}
         isLoading={false}
         entityStatus={entityStatus}
+        className={s.todolist}
       />
     );
   });
 
-  return <div className={s.mainContentBox}>{mappedTodolists}</div>;
+  return (
+    <div className={`${s.mainContentBox} ${align ? s[align] : ""}`}>
+      {mappedTodolists}
+    </div>
+  );
 };
