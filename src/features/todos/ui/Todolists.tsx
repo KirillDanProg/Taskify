1d9b4ef5d11da413fd8bb909ede9a71f7aeb019f
KirillDanProg <kirill2898v@gmail.com>
2024-02-21 18:46:34 +0300

    feat: framer motion y-axis drag and drop animation added, todolists fetched lazy in useEffect on first mount

--- src/features/todos/ui/Todolists.tsx
+++ src/features/todos/ui/Todolists.tsx
@@ -1,37 +1,50 @@
 import { Todo } from "./todo/Todo";
-import { useFetchTodoslistsQuery } from "../todoApi";
+import { useLazyFetchTodoslistsQuery } from "../todoApi";
 import s from "./Todolists.module.scss";
 import { useSearchParams } from "react-router-dom";
+import { Reorder } from "framer-motion";
+import { useEffect, useState } from "react";
+import { type TodolistType } from "features/todos/types";
 
 export const Todolists = () => {
-  const { data: todolists = [] } = useFetchTodoslistsQuery();
-  const [searchParams] = useSearchParams();
-  const sort = searchParams.get("sort");
-  const align = searchParams.get("align");
-  const sortedTodolists = sort
-    ? [...todolists].sort((a: any, b: any) => {
-        return sort === "asc"
-          ? a.title.localeCompare(b.title)
-          : b.title.localeCompare(a.title);
-      })
-    : todolists;
+  const [fetchTodolists] = useLazyFetchTodoslistsQuery();
 
-  const mappedTodolists = sortedTodolists.map(({ id, title, entityStatus }) => {
+  let [items, setItems] = useState<TodolistType[]>([]);
+  const [searchParams] = useSearchParams();
+  const sort: string | null = searchParams.get("sort");
+  const align: string | null = searchParams.get("align");
+
+  items = [] as TodolistType[];
+  useEffect(() => {
+    void (async function () {
+      const { data: todolists = [] } = await fetchTodolists();
+      setItems(todolists);
+    })();
+  }, [fetchTodolists]);
+
+  const sortedTodolists = sort
+    ? items.toSorted((a: any, b: any) => {
+        return sort === "asc" ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+      })
+    : items;
+
+  const mappedTodolists = sortedTodolists.map((todo: TodolistType) => {
+    const { id, title } = todo;
     return (
-      <Todo
-        key={id}
-        title={title}
-        id={id}
-        isLoading={false}
-        entityStatus={entityStatus}
-        className={s.todolist}
-      />
+      <Reorder.Item key={id} value={todo}>
+        <Todo key={id} title={title} id={id} isLoading={false} className={s.todolist} />
+      </Reorder.Item>
     );
   });
-
   return (
-    <div className={`${s.mainContentBox} ${align ? s[align] : ""}`}>
-      {mappedTodolists}
-    </div>
+    <Reorder.Group
+      axis="y"
+      values={items}
+      onReorder={newOrder => {
+        setItems(newOrder);
+      }}
+    >
+      <div className={`${s.mainContentBox} ${align ? s[align] : ""}`}>{mappedTodolists}</div>
+    </Reorder.Group>
   );
 };
