f0aaf6df1c9d7cac9a4e59d7e3ec5f72724c2cb4
KirillDanProg <kirill2898v@gmail.com>
2024-02-22 20:03:31 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-restricted-globals */
-const CACHE_NAME = "your-pwa-cache-v1";
-const cacheUrls = ["/index.html", "/style.css", "/index.js", "/logo.png"];
+const CACHE_NAME = 'your-pwa-cache-v1'
+const cacheUrls = ['/index.html', '/style.css', '/index.js', '/logo.png']
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) => cache.addAll(cacheUrls))
-      .catch((err) => console.log("install error: " + err))
-  );
-});
+      .then(async (cache) => { await cache.addAll(cacheUrls) })
+      .catch((err) => { console.log('install error: ' + err) })
+  )
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches
       .match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
+      .then(async (response) => await (response || fetch(event.request)))
+  )
+})
